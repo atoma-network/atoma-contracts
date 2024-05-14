41d9759204e69385b1d1277bc488d23a817bdb83
porkbrain <michael@atoma.network>
2024-05-14 17:33:22 +0200

    Dealing with Solana build system

--- solana/tests/atoma.ts
+++ solana/tests/atoma.ts
@@ -1,8 +1,10 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Atoma } from "../target/types/atoma";
+import { PublicKey } from "@solana/web3.js";
+import { Toma } from "../target/types/toma";
 
-describe("atoma", () => {
+describe("Atoma network", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
@@ -13,4 +15,49 @@ describe("atoma", () => {
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
+});
+
+describe("TOMA token", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Atoma as Program<Toma>;
+
+  // Metaplex Constants
+  const METADATA_SEED = "metadata";
+  const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+
+  // Constants from our program
+  const MINT_SEED = "mint";
+
+  // Data for our tests
+  const payer = pg.wallet.publicKey;
+  const metadata = {
+    name: "Just a Test Token",
+    symbol: "TEST",
+    uri: "https://5vfxc4tr6xoy23qefqbj4qx2adzkzapneebanhcalf7myvn5gzja.arweave.net/7UtxcnH13Y1uBCwCnkL6APKsge0hAgacQFl-zFW9NlI",
+    decimals: 9,
+  };
+  const mintAmount = 10;
+  const [mint] = PublicKey.findProgramAddressSync(
+    [Buffer.from(MINT_SEED)],
+    new PublicKey(program.idl.address)
+  );
+
+  const [metadataAddress] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(METADATA_SEED),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+      mint.toBuffer(),
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+  // Test init token
+  it("initialize", async () => {});
+
+  // Test mint tokens
+  it("mint tokens", async () => {});
 });
