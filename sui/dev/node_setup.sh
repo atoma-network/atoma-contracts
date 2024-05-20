250cb1083724ebf058d5bca1e704d8bdd09d3a7e
Martin Stefcek <gcifko@gmail.com>
2024-05-20 17:24:55 +0400

    feat: one click node setup

---
+++ sui/dev/node_setup.sh
@@ -0,0 +1,47 @@
+admin_address=$(<admin)
+treasury=$(<treasury)
+chmod +x sui/*
+#curl https://sh.rustup.rs -sSf | sh -s - -y
+. "$HOME/.cargo/env"
+#apt install jq -y
+export PATH=$PATH:$(pwd)/sui
+mkdir /root/.sui
+mkdir /root/.sui/sui_config
+cp client.yaml /root/.sui/sui_config/client.yaml
+cp sui.keystore /root/.sui/sui_config/sui.keystore
+cp sui.aliases /root/.sui/sui_config/sui.aliases
+address=$(sui client new-address ed25519 --json | tail -n 6 | jq -r ".address")
+echo $address
+sui client switch --address $address
+sui client balance
+sui client switch --env testnet
+sui client faucet
+sui client faucet
+sui client faucet
+sui client faucet
+
+while :; do
+  json_array_length=$(sui client gas --json | jq length)
+  if [ "$json_array_length" -eq 4 ]; then
+    break
+  fi
+  sleep 1
+done
+
+coins=$(sui client gas --json)
+primary_coin=$(echo $coins | jq -r ".[0].gasCoinId")
+coin2=$(echo $coins | jq -r ".[1].gasCoinId")
+coin3=$(echo $coins | jq -r ".[2].gasCoinId")
+sui client merge-coin --primary-coin $primary_coin --coin-to-merge $coin2
+sui client merge-coin --primary-coin $primary_coin --coin-to-merge $coin3
+sui client switch --address $admin_token                                                                                                                                                                          sui client call --package 0x2 --module coin --function mint_and_transfer --gas-budget 10000000 --args $treasury 10000000000 $address --type-args $package::toma::TOMA
+sui client switch --address $address
+package=$(more package)
+models=$(echo $(pwd)/models)
+cd code/atoma-contracts/sui/dev/
+chmod +x ./cli
+./cli db register-node --package $package
+
+while IFS= read -r line; do
+  ./cli db add-node-to-model --package $package --model $line --echelon 1
+done < $models
