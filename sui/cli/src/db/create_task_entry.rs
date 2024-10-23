4259e08efc6864eeda8ec12e42141d95588de6f5
Jorge António <matroid@outlook.com>
2024-10-23 10:52:38 +0100

    feat: integrate cli for the new contract functionality (#104)
    * first commit
    
    * add node task subscription and unsubscription logic
    
    * task creation
    
    * resolve syntax issues
    
    * update code
    
    * resolve compilation issues
    
    * create entry function for creating new task
    
    * create entry function for creating new task
    
    * remove task owner field
    
    * add changes to Task struct
    
    * add model name verification for creating a task
    
    * first commit
    
    * add further logic
    
    * add some additional logic for sampling attestation nodes and clippy refactors
    
    * add number of compute units for node elligibility
    
    * add further logic for nodes to claim funds
    
    * resolve compilation issues
    
    * clippy
    
    * resolve issue with fee calculation outside sampling consensus
    
    * improve and review task methods
    
    * revision
    
    * revision
    
    * code cleanup and improvements
    
    * code cleanup and improvements
    
    * first commit
    
    * add start attestation dispute cli command
    
    * merge conflicts

---
+++ sui/cli/src/db/create_task_entry.rs
@@ -0,0 +1,52 @@
+use crate::{prelude::*, DB_MODULE_NAME};
+
+const ENDPOINT_NAME: &str = "create_task_entry";
+
+pub(crate) async fn command(
+    context: &mut Context,
+    role: u16,
+    model_name: Option<String>,
+    valid_until_epoch: Option<u64>,
+    optimizations: Vec<u16>,
+    security_level: Option<u16>,
+    efficiency_compute_units: u16,
+    efficiency_time_units: Option<u16>,
+    efficiency_value: Option<u64>,
+    minimum_reputation_score: Option<u8>,
+) -> Result<TransactionDigest> {
+    let active_address = context.wallet.active_address()?;
+    let atoma_package = context.unwrap_atoma_package_id();
+    let atoma_db = context.get_or_load_atoma_db().await?;
+
+    let tx = context
+        .get_client()
+        .await?
+        .transaction_builder()
+        .move_call(
+            active_address,
+            atoma_package,
+            DB_MODULE_NAME,
+            ENDPOINT_NAME,
+            vec![],
+            vec![
+                SuiJsonValue::from_object_id(atoma_db),
+                SuiJsonValue::new(role.to_string().into())?,
+                SuiJsonValue::new(model_name.into())?,
+                SuiJsonValue::new(valid_until_epoch.into())?,
+                SuiJsonValue::new(optimizations.into())?,
+                SuiJsonValue::new(security_level.into())?,
+                SuiJsonValue::new(efficiency_compute_units.into())?,
+                SuiJsonValue::new(efficiency_time_units.into())?,
+                SuiJsonValue::new(efficiency_value.into())?,
+                SuiJsonValue::new(minimum_reputation_score.into())?,
+            ],
+            None,
+            context.gas_budget(),
+            None,
+        )
+        .await?;
+
+    let tx = context.wallet.sign_transaction(&tx);
+    let resp = context.wallet.execute_transaction_must_succeed(tx).await;
+    Ok(resp.digest)
+}
