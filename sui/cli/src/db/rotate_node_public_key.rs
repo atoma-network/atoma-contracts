fe53644d037f73c7fb03711cc6660a8bf97f0874
Jorge António <matroid@outlook.com>
2025-03-11 16:19:59 +0000

    feat: add random nonce for attestation verification (#136)
    * add random nonce for attestation verification
    
    * add changes
    
    * revert back to nonce to u64
    
    * improve the logic of `rotate_node_public_key`
    
    * add reserved values for different types
    
    * address pr comments

--- sui/cli/src/db/rotate_node_public_key.rs
+++ sui/cli/src/db/rotate_node_public_key.rs
@@ -5,12 +5,19 @@ const ENDPOINT_NAME: &str = "rotate_node_public_key";
 pub(crate) async fn command(
     context: &mut Context,
     new_public_key_commitment: Vec<u8>,
-    new_tee_attestation_bytes: Vec<u8>,
+    remote_attestation_bytes: Vec<u8>,
+    key_rotation_counter: u64,
+    device_type: u16,
+    task_small_id: Option<u64>,
 ) -> Result<TransactionDigest> {
     let active_address = context.wallet.active_address()?;
     let atoma_package = context.unwrap_atoma_package_id();
     let atoma_db = context.get_or_load_atoma_db().await?;
     let (node_badge, _) = context.get_or_load_node_badge().await?;
+
+    let task_small_id = task_small_id
+        .map(|v| vec![v.to_string()])
+        .unwrap_or_default();
 
     let tx = context
         .get_client()
@@ -26,7 +33,10 @@ pub(crate) async fn command(
                 SuiJsonValue::from_object_id(atoma_db),
                 SuiJsonValue::from_object_id(node_badge),
                 SuiJsonValue::new(new_public_key_commitment.into())?,
-                SuiJsonValue::new(new_tee_attestation_bytes.into())?,
+                SuiJsonValue::new(remote_attestation_bytes.into())?,
+                SuiJsonValue::new(key_rotation_counter.to_string().into())?,
+                SuiJsonValue::new(device_type.into())?,
+                SuiJsonValue::new(task_small_id.into())?,
             ],
             None,
             context.gas_budget(),
