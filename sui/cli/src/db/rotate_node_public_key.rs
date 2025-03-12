d39ee8a7d9427772ccc75e384af02cc1a9a7ffc0
Jorge António <matroid@outlook.com>
2025-03-12 08:22:54 +0000

    remove task small id from method `rotate_node_public_key` (#138)

--- sui/cli/src/db/rotate_node_public_key.rs
+++ sui/cli/src/db/rotate_node_public_key.rs
@@ -8,16 +8,11 @@ pub(crate) async fn command(
     remote_attestation_bytes: Vec<u8>,
     key_rotation_counter: u64,
     device_type: u16,
-    task_small_id: Option<u64>,
 ) -> Result<TransactionDigest> {
     let active_address = context.wallet.active_address()?;
     let atoma_package = context.unwrap_atoma_package_id();
     let atoma_db = context.get_or_load_atoma_db().await?;
     let (node_badge, _) = context.get_or_load_node_badge().await?;
-
-    let task_small_id = task_small_id
-        .map(|v| vec![v.to_string()])
-        .unwrap_or_default();
 
     let tx = context
         .get_client()
@@ -36,7 +31,6 @@ pub(crate) async fn command(
                 SuiJsonValue::new(remote_attestation_bytes.into())?,
                 SuiJsonValue::new(key_rotation_counter.to_string().into())?,
                 SuiJsonValue::new(device_type.into())?,
-                SuiJsonValue::new(task_small_id.into())?,
             ],
             None,
             context.gas_budget(),
